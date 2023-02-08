0257224355aa2c82ca49474e88e022cad1af2b78
MichaelYoung <michael.young@flemingcollege.ca>
2023-02-08 10:43:16 -0500

    added formatting to numbers and column size

--- Lab3.py
+++ Lab3.py
@@ -27,9 +27,9 @@ def create_orders_dir(sales_csv):
     sales_dir = os.path.dirname(os.path.abspath(sales_csv))
     todays_date = date.today().isoformat
     orders_dir = os.path.join(sales_dir, f"Orders_{todays_date}")
-    if os.path.isdir(orders_dir):
+    if not os.path.isdir(orders_dir):
         os.makedirs(orders_dir)
-    return
+    return orders_dir 
 
 def proccess_sales_data(sales_csv, orders_dir):
     sales_df = pd.read_csv(sales_csv)
@@ -51,8 +51,15 @@ def export_order_to_excel(order_id, order_df, orders_dir):
     order_file = f"Order{order_id}_{customer_name}.xlsx"
     order_path = os.path.join(orders_dir, order_file)
     
-    sheet_name = f"order #{order_id}"
-    order_df.to_excel(order_path, index=False, sheet_name=sheet_name) 
+    writer = pd.ExcelWriter(order_path, engine='xlsxwriter')
+    order_df.to_excel(writer, index=False, sheet_name=f"order #{order_id}")
+    
+    workbook = writer.book
+    worksheet = writer.sheets[f"order #{order_id}"]
+    
+    money_format = workbook.add_format({'num_format': '$#,##0.00'})
+    worksheet.set_column("B:B", 18, money_format)
+    worksheet.set_column("C:C", 18, money_format)
 
 if __name__ == '__main__':
     main()
